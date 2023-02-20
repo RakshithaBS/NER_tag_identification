0d8649f4d795386646539133d7feb686d416daea
RakshithaBS <bsrakshu98@gmail.com>
2023-02-21 01:37:04 +0530

    refactor changes

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# Set environment variable for the tracking URL where the Model Registry resides
-export MLFLOW_TRACKING_URI=http://localhost:6007
-
-# Serve the production model from the model registry
-mlflow models serve -m "/home/airflow/dags/Clinical_data_pipeline/mlruns/1/81c6cb214a53453ca3cc181f5144ab64/artifacts/models"
--- mlflow_setup.sh
+++
@@ -1,2 +0,0 @@
-
-mlflow server --backend-store-uri='sqlite:///ml_experiment_1.db' --default-artifact-root="mlruns/"  --port=6007 --host=0.0.0.0
